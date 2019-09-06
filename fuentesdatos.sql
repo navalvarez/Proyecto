3370f15e625d77784e9af7962155c616721d5227
e18184 <e18184@gmail.com>
2019-09-06 18:46:48 -0400

    Add files via upload

---
+++ fuentesdatos.sql
@@ -0,0 +1,62 @@
+COPY fuentes (codigo_num, nombre) FROM stdin;
+1360	Los Alamos
+1171	Fátima A
+1172	Fátima B
+1200	Simón Bolivar
+1221	Aeropuerto A
+1310	19 de Marzo
+1030	Circunvalación A
+1160	Narciso Campero
+1251	San Luis A
+1252	San Luis B
+1100	San Bernardo B
+1480	San Jorge I
+1080	Albat
+1143	Stadium C
+1470	Simon Bolivar 2
+1260	Lourdes
+2020	Galeria Filtrante Tomatitas
+1490	San Geronimo
+1330	Pozo T5
+1420	Guadalquivir III-Juan Pablo II
+1180	Villa Busch
+1450	Narciso Campero 2
+1040	Circunvalación B
+1350	San Salvador
+1460	Aeropuerto Industrial
+1440	Pedro Antonio Flores 2
+1110	San Bernardo A
+1381	San Jacinto A
+1210	Cárcel Pública
+1230	FAB
+1130	Pedro Antonio Flores
+1121	El Constructor A
+1191	Morros Blancos A
+1430	Andaluz
+1150	Luis Espinal A
+1320	Las Barrancas
+1410	Guadalquivir II
+2030	Toma Guadalquivir- Las Tipas
+1070	Guadalquivir
+1051	Avit A
+1192	Morros Blancos B
+1222	Aeropuerto B
+1382	San Jacinto B
+1122	El Constructor B
+1400	Valle Hermoso
+1052	Avit B
+1090	Daniel Campos
+1300	1ro de Mayo
+1020	Oscar Zamora
+1060	3 de Mayo
+1390	27 de Mayo
+1141	Stadium A
+1370	Torrecillas (Pozo Vela)
+2010	Rincon de la Victoria
+1270	Anaspujio A
+1280	Anaspujio B
+1290	Luis Espinal B
+1142	Stadium B
+1010	Tomatas C
+1240	San Jorge
+\.
